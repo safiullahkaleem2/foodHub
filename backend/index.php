0db15132d0ef200687354b4d45b68bc8b2335378
safiullahkaleem2 <skaleem@students.cs.ubc.ca>
2024-04-01 14:25:53 -0700

    redirects to login page

--- backend/index.php
+++ backend/index.php
@@ -6,18 +6,24 @@ require_once('./scripts/scripts.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 $port = '5432'; 
 
-$connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;", $user, $password);
- 
-// Intilize tables
-createTables($connection);
-sampleData($connection);
+try {
+    $connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;", $user, $password);
+    
+    createTables($connection);
+    
+    sampleData($connection);
 
+    
+    header('Location: /frontend/Pages/loginpage.html');
+    exit();
+     
+} catch (PDOException $e) {
 
-
+    die("Connection failed: " . $e->getMessage());
+}
