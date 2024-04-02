9182c5e2eb3d3a16559cc978677958cc6e04400d
Jacky Feng <jfeng14@students.cs.ubc.ca>
2024-04-02 16:31:13 -0700

    randomized events

--- backend/src/queries/userdetails.php
+++ backend/src/queries/userdetails.php
@@ -1,11 +1,9 @@
 <?php
 header('Content-Type: application/json');
-require_once __DIR__ . '/../backend/scripts/databaseconnection.php';
+require_once __DIR__ . '/../../scripts/databaseconnection.php';
 
-// Query to fetch data from the database
 $statement = $connection->prepare("SELECT * FROM userdetails");
 $statement->execute();
 $userdetails = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-// Return the first name as JSON
 echo json_encode($userdetails);
