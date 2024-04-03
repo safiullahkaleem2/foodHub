f4355c179bb26e1be80ae81f1167e5fbd52c9f06
Jacky Feng <jfeng14@students.cs.ubc.ca>
2024-04-02 22:59:19 -0700

    profilepage

--- backend/src/queries/userdetails.php
+++ backend/src/queries/userdetails.php
@@ -2,7 +2,13 @@
 header('Content-Type: application/json');
 require_once __DIR__ . '/../../scripts/databaseconnection.php';
 
-$statement = $connection->prepare("SELECT * FROM userdetails");
+$statement = $connection->prepare("SELECT * 
+                                   FROM userdetails u, professionalchef p, appuser a, professionalchefskill p2
+                                   WHERE a.username = u.username 
+                                   AND a.password = u.password 
+                                   AND a.userid = p.userid 
+                                   AND p2.restaurantaffiliation = p.RestaurantAffiliation 
+                                   AND p2.restaurantlocation = p.restaurantlocation");
 $statement->execute();
 $userdetails = $statement->fetchAll(PDO::FETCH_ASSOC);
 
